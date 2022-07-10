fd94d6bba24e7e37601d2aeb3fba44f505a42a17
Sedanur Akkoyun <akkoyunsedanur@gmail.com>
2022-07-10 15:25:53 +0300

    Order Information Application with Bridge Design Pattern
    It's a simple project to understand how Bridge works. You can find more information about the application in docx file.

---
+++ Bridge Design Pattern/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    /// <summary>
+    /// sipariş vermek için bir arayüz tanımlayan implementor
+    /// </summary>
+    public interface ISiparişBilgi
+    {
+        void BilgiMesaj(string sipariş);
+    }
+
+
+    /// <summary>
+    /// siparişin verildiği restorana bilgi sağlayan abstract sınıf
+    /// </summary>
+    public abstract class Siparişİçerik
+    {
+        public ISiparişBilgi _restoran;
+
+        public abstract void Bilgi();
+    }
+
+    /// <summary>
+    /// laktozsuz ürün siparişi için oluşturulmuş RefinedAbstraction sınıfı
+    /// </summary>
+    public class LaktozsuzÜrünSipariş : Siparişİçerik
+    {
+        public override void Bilgi()
+        {
+            _restoran.BilgiMesaj("Laktoz içermeyen ürün");
+        }
+    }
+
+    /// <summary>
+    /// Glutensiz ürün siparişi için oluşturulmuş RefinedAbstraction sınıfı
+    /// </summary>
+    public class GlutensizÜrünSipariş : Siparişİçerik
+    {
+        public override void Bilgi()
+        {
+            _restoran.BilgiMesaj("Gluten içermeyen ürün");
+        }
+    }
+    
+
+    /// <summary>
+    /// restorandan sipariş verilebilen sistem için Concrete Implementor sınıfı
+    /// </summary>
+    public class AkşamYemeği : ISiparişBilgi
+    {
+        public void BilgiMesaj(string sipariş)
+        {
+            Console.WriteLine("Akşam yemeği için " + sipariş + " sipariş ediliyor.");
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+
+
+    /// <summary>
+    /// Fit Restoran adlı restoranın sipariş sistemi için Concrete Implementor sınıfı
+    /// </summary>
+    public class FitRestoran : ISiparişBilgi
+    {
+        public void BilgiMesaj(string sipariş)
+        {
+            Console.WriteLine("Fit Restoran'dan " + sipariş + " sipariş edildi.\n");
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
---
+++ Bridge Design Pattern/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Siparişİçerik _siparişVer = new LaktozsuzÜrünSipariş();
+            _siparişVer._restoran = new AkşamYemeği();
+            _siparişVer.Bilgi();
+
+            _siparişVer._restoran = new FitRestoran();
+            _siparişVer.Bilgi();
+
+            _siparişVer = new GlutensizÜrünSipariş();
+            _siparişVer._restoran = new AkşamYemeği();
+            _siparişVer.Bilgi();
+
+            _siparişVer._restoran = new FitRestoran();
+            _siparişVer.Bilgi();
+
+            Console.ReadKey();
+        }
+    }
+}
